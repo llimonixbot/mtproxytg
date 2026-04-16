8b189ee79924955aa1b3b03b178c879ab434a9c7
nellimonix <llimonix123@ya.ru>
2026-04-16 07:32:28 +0300

    feat: remove unused files and configurations

--- src/lib/constants.ts
+++
@@ -1,9 +0,0 @@
-import { Proxy } from './types';
-
-export const CHANNEL_URL = 'tg://join?invite=Nx-_kBu9X0QyNDBi';
-
-export const DEFAULT_PROXIES: Proxy[] = [];
-
-export function buildProxyLink(proxy: Proxy): string {
-  return `tg://proxy?server=${proxy.server}&port=${proxy.port}&secret=${proxy.secret}`;
-}
